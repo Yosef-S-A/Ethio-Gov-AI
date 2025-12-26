a8512aa60ad46dd9ca3eb8d664b26bafe7bef4ab
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-12-26 13:58:56 +0300

    feat (frontend): ✨ Implement auth with drizzle and better-auth

---
+++ frontend/drizzle.config.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { defineConfig } from "drizzle-kit";
+dotenv.config();
+
+console.log("Connection URL:", process.env.DATABASE_URL);
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/db/schema/*",
+  out: "./drizzle",
+  dbCredentials: {
+    host: process.env.POSTGRES_HOST as string,
+    port: Number(process.env.POSTGRES_PORT) || 5432,
+    user: process.env.POSTGRES_USER as string,
+    password: process.env.POSTGRES_PASSWORD as string,
+    database: process.env.POSTGRES_DB as string,
+    ssl: false,
+  },
+});
